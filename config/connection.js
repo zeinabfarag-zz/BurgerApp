b380c5cb21e03678cfa59d95309587bb576890b1
Zee <zzeinabfaragg@gmail.com>
2019-01-27 22:48:14 -0500

    commit

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,25 @@
 // Set up Mysql
-var mysql = require('mysql');
+var mysql = require("mysql");
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   var connection = mysql.createConnection({
-    host: 'localhost',
+    host: "localhost",
     port: 3306,
-    user: 'root',
-    password: 'password',
-    database: 'burger_db'
+    user: "root",
+    password: "password",
+    database: "burger_db"
   });
 }
 
 connection.connect(function(err) {
   if (err) {
-    console.error('error connecting: ' + err.stack);
+    console.error("error connecting: " + err.stack);
     return;
   }
 
-  console.log('connected as id ' + connection.threadId);
+  console.log("connected as id " + connection.threadId);
 });
 
 module.exports = connection;
